f02c5a65f1ac17008fc672c2500234ff1f7989dd
1716285375 <1716285375@qq.com>
2023-11-25 21:31:45 +0800

    add main window ui

---
+++ main/mainWin.py
@@ -0,0 +1,9 @@
+import sys
+from end.ui.loginWindow_ui import Ui_Form
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from end.database.mysql_db import *
+
+
+from end.database.file_process import *
